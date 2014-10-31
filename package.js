cab98f9b40b080c2af026fc81fc3c92a63f9a5f6
Sergey Chernikov <schernikov@gmail.com>
2014-10-31 15:31:08 -0700

    try without few package fields

--- package.js
+++ package.js
@@ -1,8 +1,5 @@
 Package.describe({
-  name: "schernikov:x-editable-bootstrap",
   summary: "Latest version of X-Editable for Bootstrap with wysihtml5 rich text editor",
-  version: "1.5.2_1",
-  git: "https://github.com/schernikov/meteor-x-editable-bootstrap.git"
 });
 
 Package.on_use(function (api){
